9f5bb9d7f2a93179709a03e7c28274b3ed810d7d
Nicolas Hiller <nicolas.hiller@porsche.de>
2023-10-13 16:02:19 +0200

    Adding the XCode Project

---
+++ SimulatorManager/SimulatorManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimulatorManagerApp.swift
+//  SimulatorManager
+//
+//  Created by Nicolas Hiller on 11.10.23.
+//
+
+import SwiftUI
+
+@main
+struct SimulatorManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
