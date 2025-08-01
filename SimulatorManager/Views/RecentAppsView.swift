0c6d70f9577b6fc8094733cf31434699332ed51a
Nicolas Hiller <nicolas.hiller@porsche.de>
2025-08-01 22:24:33 +0200

    add empty state

--- SimulatorManager/Views/RecentAppsView.swift
+++ SimulatorManager/Views/RecentAppsView.swift
@@ -14,29 +14,35 @@ struct RecentAppsView: View {
     
     var body: some View {
         if Settings().showRecentApps {
-            ForEach(viewModel.recentInstalledApps, id: \.id) { appChange in
-                Menu {
-                    Button {
-                        viewModel.didSelectAppDocumentFolder(for: appChange.app)
-                    } label: {
-                        Text("Documents Folder")
-                    }
-                    Button {
-                        viewModel.didSelectAppPackageFolder(for: appChange.app)
-                    } label: {
-                        Text("App Package")
-                    }
-                    if appChange.app.hasUserDefaults {
+            if !viewModel.recentInstalledApps.isEmpty {
+                Text("Recent Apps")
+                ForEach(viewModel.recentInstalledApps, id: \.id) { appChange in
+                    Menu {
                         Button {
-                            viewModel.didSelectUserDefaultsFolder(for: appChange.app)
+                            viewModel.didSelectAppDocumentFolder(for: appChange.app)
                         } label: {
-                            Text("User Defaults")
+                            Text("Documents Folder")
                         }
+                        Button {
+                            viewModel.didSelectAppPackageFolder(for: appChange.app)
+                        } label: {
+                            Text("App Package")
+                        }
+                        if appChange.app.hasUserDefaults {
+                            Button {
+                                viewModel.didSelectUserDefaultsFolder(for: appChange.app)
+                            } label: {
+                                Text("User Defaults")
+                            }
+                        }
+                    } label: {
+                        Text(appChange.app.displayName)
+                        Text(appChange.device.name + " " + appChange.device.osVersion)
                     }
-                } label: {
-                    Text(appChange.app.displayName)
-                    Text(appChange.device.name + " " + appChange.device.osVersion)
                 }
+            } else {
+                Text("No Recent Apps")
+                    .padding()
             }
         }
     }
